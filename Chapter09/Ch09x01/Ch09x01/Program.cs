290a19f741d02137ee6303c8bccfa70a5ae4133f
paulkokos <paulkokos@DESKTOP-8MQM95P>
2017-08-13 21:42:46 +0300

    Ch09x01

---
+++ Chapter09/Ch09x01/Ch09x01/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch09x01
+{
+    public abstract class MyBase
+    {
+        
+    }
+
+    internal class Myclass : MyBase
+    {
+        
+    }
+
+    public interface IMyBaseInterface
+    {
+        
+    }
+
+    internal interface IMyBaseInterface2
+    {
+        
+    }
+
+    internal interface IMyInterface : IMyBaseInterface, IMyBaseInterface2
+    {
+        
+    }
+
+    internal sealed class MyComplexClass : Myclass, IMyInterface
+    {
+        
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyComplexClass myobj = new MyComplexClass();
+            Console.WriteLine(myobj.ToString());
+            Console.ReadKey();
+        }
+    }
+}
